855ae88c158f718e63b9ef9f971fa668d098ced1
Prathmesh Ingale <prathameshingle0@gmail.com>
2022-03-09 03:31:24 +0530

    Bug_Fix_for_random_movements

--- enemy_class.py
+++ enemy_class.py
@@ -1,8 +1,8 @@
 import pygame
+import random
 from settings import *
 
-vec = pygame.math.vector
-
+vec = pygame.math.Vector2
 
 
 class Enemy:
@@ -11,14 +11,13 @@ class Enemy:
         self.grid_pos = pos
         self.starting_pos = [pos.x, pos.y]
         self.pix_pos = self.get_pix_pos()
-	self.radius = int(self.app.cell_width//2.3)
-	self.number = number
-	self.colour = self.set_colour()
-	self.direction = vec(0,0)
-	self.personality = self.set_personality()
-	self.target = None
+        self.radius = int(self.app.cell_width//2.3)
+        self.number = number
+        self.colour = self.set_colour()
+        self.direction = vec(0, 0)
+        self.personality = self.set_personality()
+        self.target = None
         self.speed = self.set_speed()
-
 
     def update(self):
         self.target = self.set_target()
@@ -32,7 +31,18 @@ class Enemy:
                             self.app.cell_width//2)//self.app.cell_width+1
         self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER +
                             self.app.cell_height//2)//self.app.cell_height+1
-    	    
+
+    def draw(self):
+        pygame.draw.circle(self.app.screen, self.colour,
+                           (int(self.pix_pos.x), int(self.pix_pos.y)), self.radius)
+
+    def set_speed(self):
+        if self.personality in ["speedy", "scared"]:
+            speed = 2
+        else:
+            speed = 1
+        return speed
+
     def set_target(self):
         if self.personality == "speedy" or self.personality == "slow":
             return self.app.player.grid_pos
@@ -45,46 +55,37 @@ class Enemy:
                 return vec(COLS-2, 1)
             else:
                 return vec(COLS-2, ROWS-2)
-                
-    def draw(self):
-       	pygame.draw.circle(self.app.screen, self.colour,(int(self.pix_pos.x), int(self.pix_pos.y)), self.radius)     
-       	
-    def set_speed(self):
-        if self.personality in ["speedy", "scared"]:
-            speed = 2
-        else:
-            speed = 1
-        return speed
-        
-        
+
     def time_to_move(self):
-    	if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
             if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
                 return True
         if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
             if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
                 return True
         return False
-    	
+
     def move(self):
-        self.direction = self.get_random_direction()
-            
-    def get_random_direction(self):
-        while True:
-            number = random.randint(-2, 1)
-            if number == -2:
-                x_dir, y_dir = 1, 0
-            elif number == -1:
-                x_dir, y_dir = 0, 1
-            elif number == 0:
-                x_dir, y_dir = -1, 0
-            else:
-                x_dir, y_dir = 0, -1
-            next_pos = vec(self.grid_pos.x + x_dir, self.grid_pos.y + y_dir)
-            if next_pos not in self.app.walls:
-                break
-        return vec(x_dir, y_dir)
-            
+        if self.personality == "random":
+            self.direction = self.get_random_direction()
+        if self.personality == "slow":
+            self.direction = self.get_path_direction(self.target)
+        if self.personality == "speedy":
+            self.direction = self.get_path_direction(self.target)
+        if self.personality == "scared":
+            self.direction = self.get_path_direction(self.target)
+
+    def get_path_direction(self, target):
+        next_cell = self.find_next_cell_in_path(target)
+        xdir = next_cell[0] - self.grid_pos[0]
+        ydir = next_cell[1] - self.grid_pos[1]
+        return vec(xdir, ydir)
+
+    def find_next_cell_in_path(self, target):
+        path = self.BFS([int(self.grid_pos.x), int(self.grid_pos.y)], [
+                        int(target[0]), int(target[1])])
+        return path[1]
+
     def BFS(self, start, target):
         grid = [[0 for x in range(28)] for x in range(30)]
         for cell in self.app.walls:
@@ -116,28 +117,44 @@ class Enemy:
                     target = step["Current"]
                     shortest.insert(0, step["Current"])
         return shortest
-        
+
+    def get_random_direction(self):
+        while True:
+            number = random.randint(-2, 1)
+            if number == -2:
+                x_dir, y_dir = 1, 0
+            elif number == -1:
+                x_dir, y_dir = 0, 1
+            elif number == 0:
+                x_dir, y_dir = -1, 0
+            else:
+                x_dir, y_dir = 0, -1
+            next_pos = vec(self.grid_pos.x + x_dir, self.grid_pos.y + y_dir)
+            if next_pos not in self.app.walls:
+                break
+        return vec(x_dir, y_dir)
 
     def get_pix_pos(self):
-        return vec((self.grid_pos.x * self.app.cell_width) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_width // 2, (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_height // 2)
-        
+        return vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+self.app.cell_width//2,
+                   (self.grid_pos.y*self.app.cell_height)+TOP_BOTTOM_BUFFER//2 +
+                   self.app.cell_height//2)
+
     def set_colour(self):
-	if self.number == 0:
-	    return (43, 78, 203)
-	if self.number == 1:
-	    return (189, 29, 29)
-	if self.number == 2:
-	    return (197, 200, 27)
-	if self.number == 3:
-	    return (215, 159, 33)    
-	    
-	    
+        if self.number == 0:
+            return (43, 78, 203)
+        if self.number == 1:
+            return (197, 200, 27)
+        if self.number == 2:
+            return (189, 29, 29)
+        if self.number == 3:
+            return (215, 159, 33)
+
     def set_personality(self):
-	if self.number == 0:
-	    return "speedy"
-	elif self.number == 1:
-	    return "slow"
-	elif self.number == 2:
-	    return "random"
-	else:
-	    return "scared"
+        if self.number == 0:
+            return "speedy"
+        elif self.number == 1:
+            return "slow"
+        elif self.number == 2:
+            return "random"
+        else:
+            return "scared"
